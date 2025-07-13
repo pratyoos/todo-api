7c325b17556a99d4fc225819de19f604e4c8ac93
Pratyoos Panta <pratyoos.panta@outlook.com>
2025-07-13 13:29:37 +0545

    feat: auth routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,4 +7,8 @@ import { registerUser, loginUser } from '../controllers/auth.controller.js';
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 
+router.get('/health', (req, res) => {
+    res.status(200).json({ message: 'Auth service is healthy' });
+});
+
 export default router;
